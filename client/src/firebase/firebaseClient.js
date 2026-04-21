87ce135af8d7ce809e88ac0a9c2218539306c03b
sara <saraamrani1342@gmail.com>
2026-04-21 20:05:31 +0300

    update firebase

--- client/src/firebase/firebaseClient.js
+++ client/src/firebase/firebaseClient.js
@@ -6,15 +6,15 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDKMOJVqkg73X9AyCwfHAPdBoiT1LFzdt8",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "sc-cosmetics-7290a.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "sc-cosmetics-7290a",
-  storageBucket:
-    import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "sc-cosmetics-7290a.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "426819189556",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:426819189556:web:acda73c98ea6fdb129c361",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-MS83NJ1Q7Y",
+  apiKey: "AIzaSyBAwVcwfKWCZMwRF-Xnz7t-xil1KApd2oI",
+  authDomain: "final-project-43749.firebaseapp.com",
+  projectId: "final-project-43749",
+  storageBucket: "final-project-43749.firebasestorage.app",
+  messagingSenderId: "519257317404",
+  appId: "1:519257317404:web:261c0c55a9349663e43fd9",
+  measurementId: "G-XLYH0MJM6N"
 };
+
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseApp);
